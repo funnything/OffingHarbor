ebc8cec3e9998e10ae18309f66c8a6b175f41036
funnything <coding.while.sleep@gmail.com>
2013-12-28 19:29:16 +0900

    Create new action

---
+++ src/HelloAction.java
@@ -0,0 +1,11 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by toyama.yosaku on 13/12/28.
+ */
+public class HelloAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
